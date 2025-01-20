27b8a92d2a804de3a2dbc194a05297845572737d
Arran Ellmer <133149550+arraneeee@users.noreply.github.com>
2025-01-20 23:36:06 +0000

    .

---
+++ AlbumV/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlbumV.Core;
+
+namespace AlbumV.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject 
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand UploadViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public UploadViewModel UploadVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        { 
+            HomeVM = new HomeViewModel();
+            UploadVM = new UploadViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            { 
+                CurrentView = HomeVM;
+            });
+
+            UploadViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = UploadVM;
+            });
+        }
+    }
+}
---
+++ AlbumV/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Windows;
+using System.Text.Json;
+using System.Collections.ObjectModel;
+using System;
+
+
+namespace AlbumV
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Albums> Album { get; set; }
+        public MainWindow()
+        {
+            LoadJsonData();
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void LoadJsonData()
+        {
+            string jsonFilePath = "A:\\Projects\\AlbumV\\AlbumV\\JSON\\albumData.json";
+            if (File.Exists(jsonFilePath))
+            {
+                string json = File.ReadAllText(jsonFilePath);
+                Album = JsonSerializer.Deserialize<ObservableCollection<Albums>>(json);
+
+                // Debugging output
+                Console.WriteLine($"Loaded {Album?.Count} albums.");
+            }
+            else
+            {
+                Console.WriteLine($"JSON file not found at: {jsonFilePath}");
+            }
+        }
+    }
+}
