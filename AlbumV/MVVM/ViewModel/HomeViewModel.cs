87c2903c8ca19584d344cc59ec7bbab0ec419ded
Harv <86970698+Hxrv3y@users.noreply.github.com>
2025-01-20 23:59:19 +0000

    Revert "Update HomeViewModel.cs"
    This reverts commit 04baac323cebe8bb4ae891728c7a9c8a31c8550d.

--- AlbumV/MVVM/ViewModel/HomeViewModel.cs
+++ AlbumV/MVVM/ViewModel/HomeViewModel.cs
@@ -3,39 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.ObjectModel;
-using AlbumV.Core;
-using System.IO;
-using System.Text.Json;
 
 namespace AlbumV.MVVM.ViewModel
 {
-    class HomeViewModel : ObservableObject
+    class HomeViewModel
     {
-        private ObservableCollection<Albums> _album;
-        public ObservableCollection<Albums> Album
-        {
-            get { return _album; }
-            set
-            {
-                _album = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public HomeViewModel()
-        {
-            LoadJsonData();
-        }
-
-        private void LoadJsonData()
-        {
-            string jsonFilePath = "A:\\Projects\\AlbumV\\AlbumV\\JSON\\albumData.json";
-            if (File.Exists(jsonFilePath))
-            {
-                string json = File.ReadAllText(jsonFilePath);
-                Album = JsonSerializer.Deserialize<ObservableCollection<Albums>>(json);
-            }
-        }
     }
 }
