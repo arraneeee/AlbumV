d4c473322dd7f3896857135758e2e6e5f55d5345
Harv <86970698+Hxrv3y@users.noreply.github.com>
2025-01-21 00:17:23 +0000

    Update Albums.cs

--- AlbumV/Albums.cs
+++ AlbumV/Albums.cs
@@ -1,29 +1,16 @@
-﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace AlbumV.MVVM.ViewModel
+namespace AlbumV
 {
-    public class HomeViewModel
+    public class Albums
     {
-        public ObservableCollection<Albums> Albums { get; set; }
-
-        public HomeViewModel()
-        {
-            LoadAlbums();
-        }
-
-        private void LoadAlbums()
-        {
-            var json = File.ReadAllText("A:\\Projects\\AlbumV\\AlbumV\\JSON\\albumData.json");
-            Albums = JsonConvert.DeserializeObject<ObservableCollection<Albums>>(json);
-
-            // Debugging: Print out the loaded albums
-            foreach (var album in Albums)
-            {
-                Debug.WriteLine($"Name: {album.Name}, Artist: {album.Artist}, Rating: {album.Rating}, FilePath: {album.FilePath}");
-            }
-        }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string FilePath { get; set; }
+        public int Rating { get; set; }
     }
 }
