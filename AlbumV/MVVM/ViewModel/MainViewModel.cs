2ee4a16a58822545638814e12bee1e5751011225
Harv <86970698+Hxrv3y@users.noreply.github.com>
2025-04-25 17:39:10 +0100

    Revert "/"
    This reverts commit 6f58252670b92c1afcdee23c3fca427d30f5cbee.

--- AlbumV/MVVM/ViewModel/MainViewModel.cs
+++ AlbumV/MVVM/ViewModel/MainViewModel.cs
@@ -1,57 +1,43 @@
 ﻿using AlbumV.Core;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.ComponentModel;
 
 namespace AlbumV.MVVM.ViewModel
 {
-    class HomeViewModel : ObservableObject
+    class MainViewModel : ObservableObject 
     {
-        private ObservableCollection<Albums> _allAlbums;
-        private ObservableCollection<Albums> _albums;
-        private string _searchText;
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand UploadViewCommand { get; set; }
 
-        public ObservableCollection<Albums> Albums
+        public HomeViewModel HomeVM { get; set; }
+        public UploadViewModel UploadVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
         {
-            get { return _albums; }
+            get { return _currentView; }
             set 
             { 
-                _albums = value;
+                _currentView = value;
                 OnPropertyChanged();
             }
         }
+         
+        public MainViewModel() 
+        { 
+            HomeVM = new HomeViewModel();
+            UploadVM = new UploadViewModel();
 
-        public string SearchText
-        {
-            get { return _searchText; }
-            set
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            { 
+                CurrentView = HomeVM;
+            });
+
+            UploadViewCommand = new RelayCommand(o =>
             {
-                _searchText = value;
-                OnPropertyChanged();
-                FilterAlbums();
-            }
-        }
-
-        public HomeViewModel()
-        {
-            _allAlbums = new ObservableCollection<Albums>();
-
-            Albums = new ObservableCollection<Albums>(_allAlbums);
-        }
-
-        private void FilterAlbums()
-        {
-            if (string.IsNullOrEmpty(SearchText))
-            {
-                Albums = new ObservableCollection<Albums>(_allAlbums);
-            }
-            else
-            {
-                var filtered = _allAlbums.Where(a => 
-                    a.Name.ToLower().Contains(SearchText.ToLower()) || 
-                    a.Artist.ToLower().Contains(SearchText.ToLower())).ToList();
-                Albums = new ObservableCollection<Albums>(filtered);
-            }
+                CurrentView = UploadVM;
+            });
         }
     }
 }
